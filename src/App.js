bf4c687321a3066b23a822ded7fab7eb48d3b63f
EduardMat <128369415+EduardMat@users.noreply.github.com>
2023-10-31 18:51:19 +0000

    axios added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import logo from "./logo.png"
+import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <div className="wrapping">
       <header className="App-header">
-        <img src ={logo} classname="App-logo" alt="logo" width="500" height={220}/>
+        <img src ={logo} className="App-logo" alt="logo" width="500" height={220}/>
         <br/>
-        Hello
+      
       </header>
+      <main>
+
+        <Dictionary/>
+      </main>
+      <footer className="text-center">Coded by Sefora Noata 
+
+      </footer>
       </div>
+    </div>
     </div>
   );
 }
